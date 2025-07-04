523e0470c9b8229570bddd7448a968267673c7d5
Rio <acosta.hierba@gmail.com>
2025-07-04 18:09:38 +0200

    Basic Pom and project structure

---
+++ src/test/java/S5_02_Virtual_Pet_App/S502VirtualPetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package S5_02_Virtual_Pet_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S502VirtualPetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
